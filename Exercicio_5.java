60ae8c885f614ae877965de5c2104c195993c898
Arnaldo Mendes <64158563+mess-21@users.noreply.github.com>
2020-05-25 15:27:38 -0300

    Update Exercicio_5.java

--- Exercicio_5.java
+++ Exercicio_5.java
@@ -8,20 +8,20 @@ public class Exercicio_5
 	{
 		Scanner leitor = new Scanner(System.in);
 		int matricula;
-		double n1, n2, n3, n4, media;
+		double n1, n2, n3, n4, n5, media;
 		
-		System.out.println("Digite a matrÌcula do aluno");
+		System.out.println("Digite a matr√≠cula do aluno");
 		matricula = leitor.nextInt();
 		
-		System.out.println("Digite a 1™ nota do aluno (Valor de 0 a 10)");
+		System.out.println("Digite a 1¬™ nota do aluno (Valor de 0 a 10)");
 		n1 = leitor.nextDouble();		
-		System.out.println("Digite a 2™ nota do aluno (Valor de 0 a 10)");
+		System.out.println("Digite a 2¬™ nota do aluno (Valor de 0 a 10)");
 		n2 = leitor.nextDouble();
 		
-		System.out.println("Digite a 3™ nota do aluno (Valor de 0 a 10)");
+		System.out.println("Digite a 3¬™ nota do aluno (Valor de 0 a 10)");
 		n3 = leitor.nextDouble();
 		
-		System.out.println("Digite a 4™ nota do aluno (Valor de 0 a 10)");
+		System.out.println("Digite a 4¬™ nota do aluno (Valor de 0 a 10)");
 		n4 = leitor.nextDouble();
 		
 		media = (n1 + n2 + n3 + n4)/4;
@@ -32,22 +32,22 @@ public class Exercicio_5
 		}
 		else
 		{	
-			System.out.println("MatrÌcula: "+matricula);
-			System.out.println("MÈdia: "+media);
+			System.out.println("Matr√≠cula: "+matricula);
+			System.out.println("M√©dia: "+media);
 		
 			if (media >= 7) 
 			{
-				System.out.println("O aluno est· APROVADO");
+				System.out.println("O aluno est√° APROVADO");
 			}
 		
 			else if(media > 7 && media >= 3.5) 
 			{
-				System.out.println("O aluno est· em RECUPERA«√O");	
+				System.out.println("O aluno est√° em RECUPERA√á√ÉO");	
 			}
 		
 			else if(media < 3.5) 
 			{
-				System.out.println("O aluno est· REPROVADO");	
+				System.out.println("O aluno est√° REPROVADO");	
 			}
 		}
 		
